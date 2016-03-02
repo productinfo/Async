656dc1a2a878c00bbe91b52a48ea521e65a5c87f
Zhixuan Lai <zhxnlai@gmail.com>
2016-03-02 10:10:29 -0800

    adding examples

--- Pod/Classes/Extensions.swift
+++ Pod/Classes/Extensions.swift
@@ -8,4 +8,13 @@
 
 import UIKit
 
+public extension UIView {
+    class func animateWithDurationAsync(duration: NSTimeInterval, animations: () -> Void) -> (Bool -> Void) -> Void {
+        return async {
+            await(.Main) {callback in
+                UIView.animateWithDuration(duration, animations: animations, completion: callback)
+            }
+        }
+    }
+}
 
